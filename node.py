ff7b1e2113e194acfb3a0e07a2d9a0faf8ec1e3e
Osher Lerner <oshlern@nuevaschool.org>
2019-02-26 09:39:30 -0800

    added fitness function

--- node.py
+++ node.py
@@ -1,13 +1,13 @@
 ## Class to describe node interactions
 
 class Node:
-    def __init__(self, strategy, first_name,last_name):
+    def __init__(self, strategy, first_name, last_name):
         self.strategy = strategy
         self.first_name = first_name
         self.last_name = last_name
         self.neighbors = []
 
-    def add_neighbor(self,neighbor):
+    def add_neighbor(self, neighbor):
         self.neighbors.append(neighbor)
 
 
