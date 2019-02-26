ec157bbfc4d9476fda50630405f7c6ae1ab112cd
Shaheenthebean <shacull@nuevaschool.org>
2019-02-26 09:31:38 -0800

    Made some basic environment stuff.

---
+++ environment.py
@@ -0,0 +1,12 @@
+class Environment:
+    def __init__(self,nodes):
+        self.nodes = nodes
+
+    def fitness(self):
+        return 1
+
+    def selectNode(self):
+        return nodes[0]
+
+    def rebirth(node):
+        return node
