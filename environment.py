b0a44d09b0179e7f18ec2a84be1db01d9f532a62
Osher Lerner <oshlern@nuevaschool.org>
2019-02-28 00:23:17 -0800

    bug fixes, betterments, and migrated to networkx node representation

--- environment.py
+++ environment.py
@@ -1,74 +1,133 @@
 import random
-from node import *
 import networkx as nx
+import numpy as np
 
 first_names = ["Liam", "Emma", "Noah", "Olivia", "William", "Ava", "James", "Isabella", "Logan", "Sophia", "Benjamin", "Mia", "Mason", "Charlotte", "Elijah", "Amelia", "Oliver", "Evelyn", "Jacob", "Abigail", "Lucas", "Harper", "Michael", "Emily", "Alexander", "Elizabeth", "Ethan", "Avery", "Daniel", "Sofia", "Matthew", "Ella", "Aiden", "Madison", "Henry", "Scarlett", "Joseph", "Victoria", "Jackson", "Aria", "Samuel", "Grace", "Sebastian", "Chloe", "David", "Camila", "Carter", "Penelope", "Wyatt", "Riley", "Jayden", "Layla", "John", "Lillian", "Owen", "Nora", "Dylan", "Zoey", "Luke", "Mila", "Gabriel", "Aubrey", "Anthony", "Hannah", "Isaac", "Lily", "Grayson", "Addison", "Jack", "Eleanor", "Julian", "Natalie", "Levi", "Luna", "Christopher", "Savannah", "Joshua", "Brooklyn", "Andrew", "Leah", "Lincoln", "Zoe", "Mateo", "Stella", "Ryan", "Hazel", "Jaxon", "Ellie", "Nathan", "Paisley", "Aaron", "Audrey", "Isaiah", "Skylar", "Thomas", "Violet", "Charles", "Claire", "Caleb", "Bella", "Josiah", "Aurora", "Christian", "Lucy", "Hunter", "Anna", "Eli", "Samantha", "Jonathan", "Caroline", "Connor", "Genesis", "Landon", "Aaliyah", "Adrian", "Kennedy", "Asher", "Kinsley", "Cameron", "Allison", "Leo", "Maya", "Theodore", "Sarah"]
 last_names = ["Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor", "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin", "Thompson", "Garcia", "Martinez", "Robinson", "Clark", "Rodriguez", "Lewis", "Lee", "Walker", "Hall", "Allen", "Young", "Hernandez", "King", "Wright", "Lopez", "Hill", "Scott", "Green", "Adams", "Baker", "Gonzalez", "Nelson", "Carter", "Mitchell", "Perez", "Roberts", "Turner", "Phillips", "Campbell", "Parker", "Evans", "Edwards", "Collins", "Stewart", "Sanchez", "Morris", "Rogers", "Reed", "Cook", "Morgan", "Bell", "Murphy", "Bailey", "Rivera", "Cooper", "Richardson", "Cox", "Howard", "Ward", "Torres", "Peterson", "Gray", "Ramirez", "James", "Watson", "Brooks", "Kelly", "Sanders", "Price", "Bennett", "Wood", "Barnes", "Ross", "Henderson", "Coleman", "Jenkins", "Perry", "Powell", "Long", "Patterson", "Hughes", "Flores", "Washington", "Butler", "Simmons", "Foster", "Gonzales", "Bryant", "Alexander", "Russell", "Griffin", "Diaz", "Hayes"]
+possible_strategies = ['A', 'B']
+
 
 class Environment:
-	def __init__(self, w, mutation_rate, payoff_matrix, nx_graph):
-		self.initialize_nodes(nx_graph)
+	def __init__(self, graph, payoff_matrix, mutation_rate, w):
+		self.graph = graph
+		self.assert_graph(graph)
 		self.w = w
 		self.mutation_rate = mutation_rate
 		self.payoff_matrix = payoff_matrix
-		self.graph = None
+
+	def assert_graph(self, graph): # Ensure the graph has all the needed information
+		for node in graph.nodes:
+			assert 'strategy' in graph[node]
+			assert graph[node]['strategy'] in possible_strategies
+			assert 'name' in graph[node]
+			assert type(graph[node][name]) == str
 
 	def __repr__(self):
-		return [self.graph, self.nodes]
+		return "graph: {}, payoff: {}, w: {}, mutation: {}".format(self.graph, self.payoff_matrix, self.w, self.mutation_rate)
 
 	def fitness(self, node):
 		payoff = 0
-		for neighbor in node.neighbors:
-			payoff += self.payoff_matrix[node.strategy][neighbor.strategy]
-		return 1 + self.w * payoff
+		for neighbor in self.graph.neighbors(node):
+			payoff += self.payoff_matrix[self.graph[node]['strategy']][self.graph[neighbor]['strategy']]
+		f = 1 + self.w * payoff
+		return max(0, f)
 
 	def select_node(self): # Randomly selects node
 		return random.choice(nodes)
 
 	def rebirth(self, node):
 		parent = self.select_parent()
-		node.last_name = parent.last_name
-		mutate = random.random()
-		if mutate < self.mutation_rate:
-			node.change_strategy(random.choice([0,1])) # Change to random strategy
+		self.graph[node]['last_name'] = self.graph[parent]['last_name']
+		if random.random() < self.mutation_rate:
+			self.graph[node]['strategy'] = random.choice(possible_strategies) # Change to random strategy
 		else:
-			node.change_strategy(parent.strategy) # Change to parent's strategy
+			self.graph[node]['strategy'] = self.graph[parent]['strategy'] # Change to parent's strategy
 
 	def select_parent(self): # Still to do
-		node_list = []
-		for node in nodes:
-			node_list += [node] * self.fitness(node)
-			print([self.fitness(node),node.get_strategy()])
-		return random.choice(node_list)
+		fs = [self.fitness(node) for node in self.graph.nodes]
+		total_f = sum(fs)
+		ps = [f/total_f for f in fs] # probability distribution sums to 1
+			# print([self.fitness(node),self.graph[node]['strategy'])
+		return np.random.choice(list(self.graph.nodes), p=ps)
 
 	def update(self): # Moves the environment onto the next time
 		replaced = self.select_node()
 		self.rebirth(replaced)
 
-	def initialize_nodes(self, graph): # Only generates neighbors (not names or strategies)
-		# Connect nodes
-		nodes = {graph_id: Node() for graph_id in graph.nodes}
-		for graph_id in nodes:
-			nodes[graph_id].neighbors = [nodes[neighbor_id] for neighbor_id in graph.neighbors(graph_id)]
-		self.nodes = list(nodes.values())
-		# TODO: Generate names
-		surname_idxs = list(range(len(last_names)))
-		random.shuffle(surname_idxs)
-		for i, node in enumerate(self.nodes):
-			if i < len(last_names):
-				node.last_name = last_names[surname_idxs[i]]
-			else:
-				node.last_name = str(i)
-			node.first_name = random.choice(first_names)
-		# TODO: Set strategies
+def make_graph(node_ids, edges, strategies, first_names, last_names):
+	graph = nx.Graph()  #nx.complete_graph(5)
+	graph.add_nodes_from(node_ids)
+	graph.add_edges_from(edges)
+	attributes = {
+		node_id: {
+			'strategy':		strategies[node_id],
+			'first_name':	first_names[node_id],
+			'last_name':	last_names[node_id]
+		} for node_id in node_ids}
+	graph.set_node_attributes(attributes)
 
-	def generate_graph(self):
-		graph = nx.Graph()
-		graph.add_nodes_from(range(len(self.nodes)))
+# Generate random names
+def random_names(node_ids):
+	first_names, last_names = {}, {}
+	surname_idxs = list(range(len(last_names)))
+	random.shuffle(surname_idxs)
+	for i, node_id in enumerate(node_ids):
+		first_names[node_id] = random.choice(first_names)
+		if i < len(last_names):
+			last_names[node_id] = last_names[surname_idxs[i]]
+		else: # out of last names, so give up and just use numbers
+			last_names[node_id] = "surname" + str(i)
+	return first_names, last_names
 
-		edges = []
-		for i, node in enumerate(self.nodes):
-			edges += [(i, self.nodes.index(neighbor)) for neighbor in node.neighbors]
-		graph.add_edges_from(edges)
+def random_strategies(node_ids):
+	return {node_id: random.choice(possible_strategies) for node_id in node_ids}
 
-		return graph
+def balanced_strategies(node_ids):
+	strategies = {}
+	num_per_strat = int(len(node_ids)/len(possible_strategies))
+	for i, strategy in enumerate(possible_strategies):
+		for node_id in node_ids[i*num_per_strat : (i+1)*num_per_strat]:
+			strategies[node_id] = strategy
+	return strategies
+
+
+# node_ids are unique ids for the networkx graph to index its nodes
+sample_node_ids = [
+	'Osher',
+	'Ethan',
+	'Max',
+	'Shaheen',
+	'X'
+]
+
+sample_edges = [
+	('Ethan',	'Max'),
+	('Ethan', 	'Shaheen'),
+	('Osher',	'Max'),
+	('Osher',	'Ethan'),
+	('Max',		'X')
+]
+
+sample_first_names = {
+	'Osher':	'Osher',
+	'Ethan':	'Ethan',
+	'Max':		'Max',
+	'Shaheen',	'Shaheen',
+	'X':		'Bippity'
+}
+
+sample_last_names = {
+	'Osher':	'Lerner',
+	'Ethan':	'Knight',
+	'Max':		'Gotts',
+	'Shaheen',	'Cullen-Baratloo',
+	'X':		'Bop'
+}
+
+sample_strategies = {
+	'Osher':	'A',
+	'Ethan':	'B',
+	'Max':		'A',
+	'Shaheen',	'B',
+	'X':		'B'
+}
--- program.py
+++ program.py
@@ -1,9 +1,15 @@
-from node import *
 from environment import *
 
+a, b, c, d = 1, 2, 3, 4
+payoff_matrix = {'A': {'A': a, 'B', b}, 'A': {'A': c, 'B', d}}
 
-g=nx.Graph()
-n_nodes = 10
-for i in range(n_nodes):
-    g.add_edge(i, (i + 1) % n_nodes)
-e = Environment(0.05,0.05,[[1,2],[3,4]],g)
+# Make graph
+n = 10
+node_ids = range(n)
+edges = [(i, (i+1) % n) for node in node_ids]
+first_names, last_names = random_names(node_ids)
+strategies = balanced_strategies(node_ids) # alternatively, use random_strategies
+graph = make_graph(node_ids, edges, strategies, first_names, last_names)
+# graph = make_graph(sample_node_ids, sample_edges, sample_strategies, sample_first_names, sample_last_names)
+
+env = Environment(graph, payoff_matrix=payoff_matrix, mutation_rate=0.05, w=0.05)
