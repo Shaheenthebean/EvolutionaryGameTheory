f24ce55bac588997e69d6c7c6e3170e4351a5cfb
Shaheenthebean <shacull@nuevaschool.org>
2019-02-27 23:23:18 -0800

    ?

--- program.py
+++ program.py
@@ -1,17 +1,19 @@
 from node import *
 from environment import *
 
-sugma=2.2
-mat = [[1,2],[3,4]]
+mat = [[1,2],[-5,4]]
 g=nx.Graph()
-n_nodes = 10
+n_nodes = 100
+sugma = (3*n_nodes-8)/n_nodes
+print(sugma*mat[0][0]+mat[0][1])
+print(mat[1][0]+sugma*mat[1][1])
 for i in range(n_nodes):
     g.add_edge(i, (i + 1) % n_nodes)
-e = Environment(0.05,0.05,[[1,2],[3,4]],g)
+e = Environment(0.0050,0.005,mat,g)
 
 t = []
 for j in range(100):
-    for i in range(10000):
+    for i in range(100000):
         e.update()
 
 
